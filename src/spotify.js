156739f8f5a837b33b7e0ef871ce8c51da30c9f4
samuel <wonho.jung92@gmail.com>
2022-12-06 12:10:09 -0500

    changed  redirect url

--- src/spotify.js
+++ src/spotify.js
@@ -1,6 +1,5 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-// const redirectUrl = "https://spotify-clone-c80b5.web.app/";
-const redirectUrl = "http://localhost:3000/";
+const redirectUrl = "https://subtle-jalebi-49df22.netlify.app/";
 const clientId = "0bddb0bd65e7460b8c9e2ece59e974e3";
 
 const scopes = [
