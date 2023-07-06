60be0b08d277f71f67875ea1af14b9589d779e8c
samuel <wonho.jung92@gmail.com>
2023-07-06 18:32:18 -0400

    bug fix

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const apiServer = axios.create({
-  // baseURL: process.env.REACT_APP_API_URL,
-  baseURL: "http://localhost:8888/.netlify/functions/api",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export function createPlaylist(data) {
