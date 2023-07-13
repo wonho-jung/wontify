5fe85ac3f2947f0febac82b0882a772f4c7def87
samuel <wonho.jung92@gmail.com>
2023-07-13 14:33:36 -0400

    testing

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -11,7 +11,6 @@ function Artist({ spotify }) {
   const artistDetail = useSelector(selectArtistDetail);
   const [followNumber, setFollowNumber] = useState("");
   const [btntext, setBtntext] = useState("Follow");
-  console.log("eslint test with husky");
   useEffect(() => {
     if (artistDetail) {
       setFollowNumber(artistDetail.artistInfo.followers.total);
--- src/components/DetailAlbum.js
+++ src/components/DetailAlbum.js
@@ -35,6 +35,8 @@ function DetailAlbum({ spotify }) {
   }, []);
 
   return (
+    test catch bug
+
     <DetailAlbumContainer>
       {loading ? (
         <Loading />
